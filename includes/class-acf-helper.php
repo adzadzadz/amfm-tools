cfd6176e3570e3a1d3cd02ab2e72e1c4d7a32dbb
Adrian T. Saycon <adzbite@gmail.com>
2025-06-18 17:26:14 +0800

    Save to cookie

--- includes/class-acf-helper.php
+++ includes/class-acf-helper.php
@@ -16,6 +16,33 @@ class ACF_Helper {
 
     // run method
     public static function run() {
-        // Code to run the ACF helper
+        // run set_keywords_to_cookies method on all post and pages
+        add_action( 'wp', [ __CLASS__, 'set_keywords_to_cookies' ] );
+    }
+    
+    public static function set_keywords_to_cookies() {
+        // Get the keywords from the ACF field
+        $keywords = self::get_keywords();
+
+        // Set the keywords in cookies
+        if ( ! empty( $keywords['keywords'] ) ) {
+            setcookie( 'amfm_keywords', json_encode( $keywords['keywords'] ), time() + 3600, COOKIEPATH, COOKIE_DOMAIN );
+        }
+
+        if ( ! empty( $keywords['other_keywords'] ) ) {
+            setcookie( 'amfm_other_keywords', json_encode( $keywords['other_keywords'] ), time() + 3600, COOKIEPATH, COOKIE_DOMAIN );
+        }
+    }
+
+    public static function get_keywords() {
+        // Get the keywords from the ACF field
+        $keywords = get_field( 'amfm_keywords' );
+        $otherKeywords = get_field( 'amfm_other_keywords' );
+
+        // Return the keywords as an array
+        return [
+            'keywords' => $keywords ? explode( ',', $keywords ) : [],
+            'other_keywords' => $otherKeywords ? explode( ',', $otherKeywords ) : []
+        ];
     }
 }

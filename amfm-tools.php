dd8451885f6430c26a2c10894c1f09a7972de584
Adrian T. Saycon <adzbite@gmail.com>
2025-08-09 01:56:25 +0800

    Version update

--- amfm-tools.php
+++ amfm-tools.php
@@ -3,7 +3,7 @@
 Plugin Name: AMFM Tools
 Plugin URI: https://adzbyte.com/
 Description: A plugin for AMFM custom functionalities.
-Version: 1.6.0
+Version: 1.8.0
 Author: Adrian T. Saycon
 Author URI: https://adzbyte.com/adz
 License: GPL2
